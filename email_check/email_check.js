3caac92a3faf10d259d83674cb13461785640822
rlawldns12 <101171594+rlawldns12@users.noreply.github.com>
2023-03-22 15:32:29 +0900

    이메일인증 (js 기능 미완성)

--- email_check/email_check.js
+++ email_check/email_check.js
@@ -1,16 +1,19 @@
-function moveNext(input, nextInputID) { // 매개변수 input nextinputid 선언
-    if (input.value.length == input.maxLength) { // input의 값의 길이가 최대일때
-      document.getElementById(nextInputID).focus(); // 포커스를 다음 input id를 전환
-    }
+function moveNext(input, nextInputID) {
+  if (input.value.length == input.maxLength) {
+    document.getElementById(nextInputID).focus();
   }
-function moveBack(input,BackInputID) {
-    if(input.value.length === 0 ){
-        document.getElementById(BackInputID).focus();
-    }
 }
-/*function moveBack(input, inputId) {
-    if (input.value.length === 0) {
-      document.getElementById(inputId).focus();
-    }
+
+function moveBack(input, backInputID) {
+  if (input.value.length == 0 || !input.value || event.keyCode == 8) {
+    document.getElementById(backInputID).focus();
   }
-  */
+}
+
+/*
+
+moveBack 함수 실행 대안 
+
+1. input 그냥 없애고 Backspace 키를 누르면 바로 이전 input 으로 넘김 
+2. input 값의 길이를 맞는지 체크 
+*/
