482c5d30d1c0608ad41eb5d29615a526fdc8ee21
rlawldns12 <101171594+rlawldns12@users.noreply.github.com>
2023-03-27 21:56:34 +0900

    회원가입 유효성 검사 기능 추가중

---
+++ Sign_up_page/Sign_up.js
@@ -0,0 +1,25 @@
+var  username = document.getElementById('username');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+function null_check(){
+    var  username = document.getElementById('username');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+if(username.value === ""){
+    return false;
+};
+
+if(email.value === ""){
+    return false;
+};
+
+if(password.value === ""){
+    return false;
+};
+
+if(false)
+
+
+}
